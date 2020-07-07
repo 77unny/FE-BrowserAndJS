eb6b71c44f2c2522f55597960805140f3e5c333b
baekCode <sosaer88@gmail.com>
2020-07-07 14:43:07 +0900

    feat : Set() 자료형을 사용하지 않고 등록된 데이터 업데이트

--- 1-WebAPIs/3-Project/src/resource/js/main.js
+++ 1-WebAPIs/3-Project/src/resource/js/main.js
@@ -6,15 +6,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const formElement = document.querySelector(TAG_LIST.ELEMENTS_NAMESPACE.FORM);
   const formInputElement = document.querySelector(TAG_LIST.ELEMENTS_NAMESPACE.INPUT);
-  const shoppingList = new Set();
+  const listElement = document.querySelector('.shopping-list');
   let shoppingItem = null;
 
   formInputElement.focus();
 
   formElement.addEventListener('submit', e => {
     e.preventDefault();
-    shoppingList.add(shoppingItem);
+    if (!shoppingItem) return;
+    listElement.insertAdjacentHTML(
+      'beforeend',
+      `<div class="shopping-item"><span>${shoppingItem}</span><button class="btn-del">제거</button></div>`,
+    );
     formInputElement.value = '';
+    shoppingItem = null;
   });
   formElement.addEventListener('input', e => (shoppingItem = e.target.value));
 });
