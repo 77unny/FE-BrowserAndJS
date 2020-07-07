241631d0a7601c17242735137c59c2d3aab48cb0
baekCode <sosaer88@gmail.com>
2020-07-07 13:58:35 +0900

    feat : 폼 submit 을 실행하면  input value 값을 저장

--- 1-WebAPIs/3-Project/src/resource/js/main.js
+++ 1-WebAPIs/3-Project/src/resource/js/main.js
@@ -3,4 +3,16 @@ import { LOG } from './utils.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   LOG(TAG_LIST.MAIN);
+
+  const formElement = document.querySelector('.shopping-form form');
+  const shoppingList = new Set();
+  let shoppingItem = null;
+
+  formElement.addEventListener('submit', e => {
+    e.preventDefault();
+    shoppingList.add(shoppingItem);
+  });
+  formElement.addEventListener('input', e => {
+    shoppingItem = e.target.value;
+  });
 });
