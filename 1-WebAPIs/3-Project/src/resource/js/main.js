96e42f1ddccff33495d1d02a38306cb121dcfe22
baekCode <sosaer88@gmail.com>
2020-07-07 13:59:46 +0900

    feat : 등록 이후 input 초기화

--- 1-WebAPIs/3-Project/src/resource/js/main.js
+++ 1-WebAPIs/3-Project/src/resource/js/main.js
@@ -5,14 +5,16 @@ window.addEventListener('DOMContentLoaded', () => {
   LOG(TAG_LIST.MAIN);
 
   const formElement = document.querySelector('.shopping-form form');
+  const formInputElement = document.querySelector('.shopping-form input');
   const shoppingList = new Set();
   let shoppingItem = null;
+
+  formInputElement.focus();
 
   formElement.addEventListener('submit', e => {
     e.preventDefault();
     shoppingList.add(shoppingItem);
+    formInputElement.value = '';
   });
-  formElement.addEventListener('input', e => {
-    shoppingItem = e.target.value;
-  });
+  formElement.addEventListener('input', e => (shoppingItem = e.target.value));
 });
