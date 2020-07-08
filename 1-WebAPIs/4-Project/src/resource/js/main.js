c6cdfcf01a73f42389e629a8d83181d4c680c23b
baekCode <sosaer88@gmail.com>
2020-07-08 13:50:51 +0900

    modify : 네임스페이스 적용, 코드 가독성

--- 1-WebAPIs/4-Project/src/resource/js/main.js
+++ 1-WebAPIs/4-Project/src/resource/js/main.js
@@ -1,8 +1,12 @@
 import * as Utils from './utils/utils.js';
+import * as CONSTANT from './utils/constant.js';
 import Header from './Components/Header/header.js';
 import List from './Components/List/list.js';
 import Form from './Components/Form/form.js';
 
 const root = document.querySelector('#shopping-app');
+const headerComponent = Header(CONSTANT.TITLE);
+const listComponent = List();
+const formComponent = Form();
 
-Utils.connectComponents(root, Header, List, Form);
+Utils.connectComponents(root, headerComponent, listComponent, formComponent);
